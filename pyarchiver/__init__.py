3ba23d0e31aa0304e336dce16319db612b399983
Clivern <hello@clivern.com>
2022-10-12 21:34:36 +0200

    cleanup

--- pyarchiver/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Python Compression and Archiving Library
-
-@author: Clivern U{hello@clivern.com}
-"""
-
-__VERSION__ = "1.0.0"
--- tests/test_pyarchiver.py
+++
@@ -1,5 +0,0 @@
-"""
-PyArchiver Compression and Archiving Library
-
-@author: Clivern U{hello@clivern.com}
-"""
