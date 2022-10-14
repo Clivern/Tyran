9984165f76c6f945c299ff958ec8c3290d6a43cb
Clivern <hello@clivern.com>
2022-10-14 12:58:48 +0200

    chore: adjust examples

--- testdata/load.py
+++ testdata/load.py
@@ -1,33 +1,43 @@
-import requests
+# MIT License
+#
+# Copyright (c) 2024 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Specify the API endpoint URL
-url = 'http://127.0.0.1:8000/api/v1/document'
+from pytyran.client import Client
+from pytyran.reader import Reader
 
-# Set the headers
-headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json'
-}
+
+reader = Reader()
+tyran_client = Client("http://127.0.0.1:8000")
 
 # Loop through the files
 for i in range(1, 15):
-    filename = f'testdata/runbook{i}.md'
+    filename = f"testdata/runbook{i}.md"
 
     # Read the file content
-    with open(filename, 'r') as file:
-        content = file.read()
-
-    # Prepare the request data
-    data = {
-        "content": content,
-        "metadata": {
-            "type": "runbook",
-            "team": "devops"
-        }
-    }
+    content = reader.read_file(filename)
 
     # Send the POST request
-    response = requests.post(url, headers=headers, json=data)
+    response = tyran_client.create_document(
+        content, {"type": "runbook", "team": "devops"}
+    )
 
     # Check the response status code
     if response.status_code == 200:
--- testdata/search.py
+++ testdata/search.py
@@ -1,42 +1,54 @@
+# MIT License
+#
+# Copyright (c) 2024 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import argparse
-import requests
 import json
+from pytyran.client import Client
+
 
 def main(search_text):
-    # Specify the API endpoint URL
-    url = 'http://127.0.0.1:8000/api/v1/document/search'
-
-    # Set the headers
-    headers = {
-        'accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-
-    # Prepare the request data
-    data = {
-        "text": search_text,
-        "limit": 3,
-        "metadata": {
-            "type": "runbook",
-            "team": "devops"
-        }
-    }
+    tyran_client = Client("http://127.0.0.1:8000")
 
     # Send the POST request
-    response = requests.post(url, headers=headers, json=data)
+    response = tyran_client.search_documents(
+        search_text, {"type": "runbook", "team": "devops"}, 3
+    )
 
     # Check the response status code
     if response.status_code == 200:
         print("Response from server:")
-        print(json.dumps(response.json(), indent=2))  # Pretty print the JSON response
+        # Pretty print the JSON response
+        print(json.dumps(response.json(), indent=2))
     else:
         print(f"Failed to perform search. Status code: {response.status_code}")
         print("Response text:", response.text)
 
+
 if __name__ == "__main__":
     # Set up argument parsing
-    parser = argparse.ArgumentParser(description='Search documents using the API.')
-    parser.add_argument('text', type=str, help='The text to search for in the documents.')
+    parser = argparse.ArgumentParser(description="Search documents using the API.")
+    parser.add_argument(
+        "text", type=str, help="The text to search for in the documents."
+    )
 
     # Parse the arguments
     args = parser.parse_args()
